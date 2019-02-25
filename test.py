f82d157f1cabf130794440281089a54c967b3fd8
Mikhail Nurgaliev <mikhail.nurgaliev@gs-labs.ru>
2019-02-25 08:23:20 +0300

    test

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import UrlHelpers, Order, Database
 
 
 db = Database.Database()
-products = db.get_products(100)
+products = db.get_products(10)
 sending_orders = []
 processed_orders = []
 
@@ -14,21 +14,3 @@ for product_id, sku_num in products:
     print(data)
 
 
-product_id='1711aac6-9474-4bba-b0ba-896ecd0ea719'
-prod2_id = 'dsadas'
-sku_num='197738988'
-sku2='dsadas'
-order = Order.Order(product_id, sku_num)
-print(order)
-order_data = order.data
-order2=Order.Order(prod2_id, sku2)
-print(order2.data)
-print(order2.id)
-
-
-
-response = UrlHelpers.send_order_to_wms(str(order))
-print(response.headers)
-print(response.json())
-print(response.text)
-print(response.content)
