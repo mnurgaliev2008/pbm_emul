9cc415f90ff36c44811e2db3881d0beba76511ca
Mikhail Nurgaliev <mikhail.nurgaliev@gs-labs.ru>
2019-02-26 12:59:42 +0300

    test

--- AppPBM.py
+++ AppPBM.py
@@ -8,6 +8,7 @@ app_pbm = Flask(__name__)
 executor = executor(max_workers=3)
 received_orders = []
 
+@app_pbm.route('/', defaults={'path': ''})
 @app_pbm.route('/<path:path>')
 def catch_all(path):
     print('tut')
